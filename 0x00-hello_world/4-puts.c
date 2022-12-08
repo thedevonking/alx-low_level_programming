c3faf12ba315e895c04c3c620c989569d6c27537
thedevonking <thedevonking01@gmail.com>
2022-12-08 07:10:34 -0800

    I'm trying LOL

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
 {
-	puts("\"Programming is like building multilingual puzzle");
-
-	return(0)
+puts("Programming is like building multilingual puzzle\n");
+return(0);
 }

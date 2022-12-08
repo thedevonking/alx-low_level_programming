bed65ddfce2b5cdeac0eb735d259f11cdf0382bb
thedevonking <thedevonking01@gmail.com>
2022-12-08 07:18:25 -0800

    OMO

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ int main(void)
  * Return: 0 after printing the function
  */
 {
-puts("\"Programming is like building multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

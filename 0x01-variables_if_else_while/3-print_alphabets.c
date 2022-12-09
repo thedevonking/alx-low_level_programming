3fc07e06a48e11221f0026c8426686879c09e48c
thedevonking <thedevonking01@gmail.com>
2022-12-09 13:25:49 -0800

    Learning to use putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,32 +4,31 @@
 
 /**
  *
- *  * main - prints the alphabet in lowercase, and then in uppercase,
+ * main - prints the alphabet in lowercase, and then in uppercase,
  *
- *   * followed by a new line
+ * followed by a new line
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-		int ch;
+	int ch;
 
 
 
-			for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 
-						putchar(ch);
+	putchar(ch);
 
-				for (ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 
-							putchar(ch);
+	putchar(ch);
 
-					putchar('\n');
+	putchar('\n');
 
-						return (0);
+	return (0);
 
 }

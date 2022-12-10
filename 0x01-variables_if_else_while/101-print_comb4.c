7775ba5c6fbf9c07b5d1f13237c7fe7267a42aaf
thedevonking <thedevonking01@gmail.com>
2022-12-09 19:38:58 -0800

    Almost done with Comibination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int n, m, l;
-	
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
@@ -20,7 +19,7 @@ int main(void)
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 55 || m !=56)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');

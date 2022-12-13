5d8aba926d7fb93ae24057014c0245c15cf42796
thedevonking <thedevonking01@gmail.com>
2022-12-13 11:44:48 -0800

    Advancing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 100 )) % 10 + '0');
+					_putchar((prod / 100 ) % 10 + '0');
 				}
 				else if (prod <= 99 &&  prod >= 10)
 				{

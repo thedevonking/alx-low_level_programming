f4c407049ddaa21a0db3b005c3ee88ad20e1f5b3
D'von King <117774846+thedevonking@users.noreply.github.com>
2022-12-13 23:50:54 +0100

    gg

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -20,12 +20,12 @@ int main(void)
 		{
 			sum3 = sum3 + i;
 		}
-	       	else if ((i % 5) == 0)
+		else if ((i % 5) == 0)
 		{
 			sum5 = sum5 + i;
 		}
 	}
 	sum = sum3 + sum5;
 	printf("%lu\n", sum);
-	return (0);
+	return (h0);
 }

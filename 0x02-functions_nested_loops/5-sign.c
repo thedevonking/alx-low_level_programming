306eb281a03a42f60c1d9d7c98ccec446ebe65f8
thedevonking <thedevonking01@gmail.com>
2022-12-13 09:14:19 -0800

    Absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * @n: The number of which the sign will be printed.
  *
  * Return: 1 if the number is greater than zero,
- * 	   0 if the number is zero,
- * 	  -1 if the number is less than zero.
+ *   0 if the number is zero,
+ *  -1 if the number is less than zero.
  */
 int print_sign(int n)
 {

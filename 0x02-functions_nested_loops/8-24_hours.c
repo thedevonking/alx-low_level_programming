c0a8094a8e3e43169af3ae02d01b2451b05fbf13
thedevonking <thedevonking01@gmail.com>
2022-12-13 10:04:51 -0800

    Print 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) = '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');

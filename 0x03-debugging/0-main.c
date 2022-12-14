bfb8eb2df7a366ea31224e99af3f4327b4dd9260
thedevonking <thedevonking01@gmail.com>
2022-12-14 11:04:32 -0800

    Started solving codes problems

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function taht prunts if an integer is possitive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging shouldn't be hard after all

be52d19743c4d9d74f75ed48cb62c0544d9eb43a
thedevonking <thedevonking01@gmail.com>
2022-12-15 13:05:40 -0800

    Print 10 times a number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * print_more_numbers - print
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+	}
+}

068b80ffa08cab6cccf7997924ac210006c5213b
thedevonking <thedevonking01@gmail.com>
2022-12-15 15:20:38 -0800

    FizzBuzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,9 +21,16 @@ int main(void)
 		{
 			printf(" Buzz");
 		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+			printf("%d", i);
+		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf('\n');

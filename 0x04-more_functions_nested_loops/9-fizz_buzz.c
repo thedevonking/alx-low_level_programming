b4dc88f996db9a096b3c6068181194ab439b5905
thedevonking <thedevonking01@gmail.com>
2022-12-15 15:30:03 -0800

    FizzBuzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,27 +13,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 )
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 15 == 0)
 		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
-			printf("%d", i);
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%i", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
-
 	return (0);
 }

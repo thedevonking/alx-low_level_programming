a0025815e98daa8e79e3feefe0433960ebf30faf
D'von King <117774846+thedevonking@users.noreply.github.com>
2022-12-16 01:00:49 +0100

    Debugging

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,22 @@
-#include <stdio.h>
-#include <math.h>
-
+#include "main.h"
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
- * followed by a new line
- * Return: Always 0 (Success)
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
+ *Owned by Bwave ICT/ Bright Daniel
  */
-int main(void)
+void print_number(int n)
 {
-	long int n;
-	long int max;
-	long int i;
+	unsigned int i = n;
 
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
+	if (n < 0)
 	{
-		max = 2;
-		n /= 2;
+		_putchar(45);
+		i = -i;
 	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
+	if (i / 10)
 	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
+		print_number(i / 10);
 	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
-	return (0);
+	_putchar(i % 10 + '0');
 }

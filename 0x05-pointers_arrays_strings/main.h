d30638e0870b0e5833726f8bf6ea48665897d4ac
thedevonking <thedevonking01@gmail.com>
2022-12-19 04:13:29 -0800

    first pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-voint print_array(int *a, int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);

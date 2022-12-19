919a23c0532a6a12fae94024f24e14310b6ac9e3
thedevonking <thedevonking01@gmail.com>
2022-12-19 04:34:23 -0800

    How long can the string be?

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * int_strlen - Print the lenght of a string
+ * _strlen - Print the lenght of a string
  * @s: charcter to check
  * Return: 0
  */
 int _strlen(char *s)
 {
-	int a = 0 ;
+	int a = 0;
 
 	for (; *s++;)
 			a++;

35e7e738535cee4a28896749f727b584eb90e1a7
D'von King <117774846+thedevonking@users.noreply.github.com>
2022-12-19 23:16:32 +0100

    Funally

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,27 +1,15 @@
 #include "main.h"
 /**
- * _strlen - Print the lenght of a string
- * @s: charcter to check
- * Return: 0
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 
  */
 int _strlen(char *s)
 {
-<<<<<<< HEAD
 	int a = 0;
-	
+
 	for (; *s++;)
-			a++;
+		a++;
 	return (a);
-=======
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
->>>>>>> ffd7bcf612462fcb041d069c5fa978959fa9a6fc
 }

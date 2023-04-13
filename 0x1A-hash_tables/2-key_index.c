39d3df77c2278c90f407f8837e9c850f63385323
D'von King <117774846+thedevonking@users.noreply.github.com>
2023-04-13 06:18:36 +0100

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,20 +1,15 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-#include <stddef.h>
+
 /**
- * key_index - returns the index of a key 
- * @key: key
- * @size: size of the array od the hatshtable
- * Return: index where key/value pair should be stored 
+ * key_index - Get the index at which a key/value
+ *      pair should be stored in array of a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ * Description: Uses the djb2 algorithm.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
-
-	if (key != NULL && size)
-	{
-		index = hash_djb2(key) % size;
-		return (index);
-	}
-	exit(-1);
+	return (hash_djb2(key) % size);
 }
